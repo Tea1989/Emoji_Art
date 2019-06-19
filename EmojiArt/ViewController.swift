c7cc0df65d207506fde71a2d37eed1ededc85c1c
Rautea <rautea1989@gmail.com>
2019-06-18 19:22:49 -1000

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Rautea Lionel Teihotu on 6/18/19.
+//  Copyright © 2019 Rautea Lionel Teihotu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
